ed006d7909ba1af32a8216255b6aa668c33aa7d1
chenzhiguang <zhiguang.c@gmail.com>
2022-07-05 05:23:49 +0200

    Add a test case to original

--- test/builder_test.dart
+++ test/builder_test.dart
@@ -9,11 +9,11 @@ import 'theme_data.dart';
 
 void main() {
   // _testDirectory('common_mark');
+  _testDirectory('original');
   _testDirectory('gfm');
 }
 
-// TODO(Zhiguang): try to compare the built result with the html result to
-// verify
+// TODO(Zhiguang): compare the built result with the html result to verify.
 
 void _testDirectory(String name) {
   final directory = '${_getRoot()}/$name';
@@ -23,39 +23,39 @@ void _testDirectory(String name) {
   );
 
   for (final entry in entries) {
-    if (![
-      // 'atx_headings.json',
-      // 'setext_headings.json',
-      'emphasis_and_strong_emphasis.json',
-      // 'paragraphs.json',
-      // 'block_quotes.json',
-
-      //'fenced_code_blocks.json',
-      //'indented_code_blocks.json',
-      // 'autolinks.json',
-      // 'autolinks_extension_.json',
-      // 'backslash_escapes.json',
-      // 'blank_lines.json',
-      // 'code_spans.json',
-      // 'disallowed_raw_html_extension_.json',
-      // 'entity_and_numeric_character_references.json',
-      // 'hard_line_breaks.json',
-      // 'html_blocks.json',
-      // 'images.json',
-      // 'inlines.json',
-      // 'link_reference_definitions.json',
-      // 'links.json',
-      // 'list_items.json',
-      // 'lists.json',
-      // 'precedence.json',
-      // 'raw_html.json',
-      // 'soft_line_breaks.json',
-      // 'strikethrough_extension_.json',
-      // 'tables_extension_.json',
-      // 'tabs.json',
-      // 'textual_content.json',
-      // 'thematic_breaks.json',
-    ].contains(entry.path.split('/').last)) {
+    if (name == 'gfm' &&
+        ![
+          'atx_headings.json',
+          'setext_headings.json',
+          'emphasis_and_strong_emphasis.json',
+          'paragraphs.json',
+          'block_quotes.json',
+          'fenced_code_blocks.json',
+          'indented_code_blocks.json',
+          'autolinks.json',
+          'autolinks_extension_.json',
+          'backslash_escapes.json',
+          'blank_lines.json',
+          'code_spans.json',
+          'disallowed_raw_html_extension_.json',
+          'entity_and_numeric_character_references.json',
+          'hard_line_breaks.json',
+          'html_blocks.json',
+          'images.json',
+          'inlines.json',
+          'link_reference_definitions.json',
+          'links.json',
+          'list_items.json',
+          'lists.json',
+          'precedence.json',
+          'raw_html.json',
+          'soft_line_breaks.json',
+          'strikethrough_extension_.json',
+          'tables_extension_.json',
+          'tabs.json',
+          'textual_content.json',
+          'thematic_breaks.json',
+        ].contains(entry.path.split('/').last)) {
       continue;
     }
 
@@ -71,9 +71,9 @@ void _testFile(String path) {
     final description = testCase['description'];
     final data = testCase['markdown'];
 
-    if (!['360', '378'].contains(description.split('-').last)) {
-      // continue;
-    }
+    //if (!['360', '378'].contains(description.split('-').last)) {
+    // continue;
+    //}
 
     if (expected.isEmpty) {
       continue;
