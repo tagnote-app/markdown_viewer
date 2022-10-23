c1bb4ce0976cf2bd3825e28be35daf58290f8859
chenzhiguang <zhiguang.c@gmail.com>
2022-10-23 05:25:33 +0200

    Fix widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,54 +4,56 @@ import 'package:markdown_viewer/markdown_viewer.dart';
 
 void main() {
   testWidgets('when empty', ((tester) async {
-    await tester.pumpWidget(
-      const Directionality(
-        textDirection: TextDirection.ltr,
-        child: MarkdownViewer(''),
-      ),
-    );
+    await tester.pumpWidget(_createWidget(''));
 
-    final allWidgets = tester.allWidgets;
-
-    expect(allWidgets.elementAt(0).runtimeType, Directionality);
-    expect(allWidgets.elementAt(1).runtimeType, MarkdownViewer);
-    expect(allWidgets.elementAt(2).runtimeType, SizedBox);
-    final sizedBox = allWidgets.elementAt(2) as SizedBox;
-    expect(
-      {'width': sizedBox.width, 'height': sizedBox.height},
-      {'width': 0.0, 'height': 0.0},
-    );
+    final childWidgets = _descendant();
+    expect(childWidgets.elementAt(0).runtimeType, SelectableRegion);
+    expect(childWidgets.last.toString(), const SizedBox.shrink().toString());
   }));
 
   testWidgets('when has only one block child', ((tester) async {
-    await tester.pumpWidget(
-      const Directionality(
-        textDirection: TextDirection.ltr,
-        child: MarkdownViewer('Foo'),
-      ),
-    );
+    await tester.pumpWidget(_createWidget('Foo'));
 
-    final allWidgets = tester.allWidgets;
+    final childWidgets = _descendant();
+    final lastIndex = childWidgets.length - 1;
 
     // This column is from MarkdownBuilder.
-    expect(allWidgets.elementAt(2).runtimeType, Column);
-    expect(allWidgets.elementAt(3).runtimeType, RichText);
+    expect(childWidgets.elementAt(lastIndex - 1).runtimeType, Column);
+    expect(childWidgets.elementAt(lastIndex).runtimeType, RichText);
   }));
 
   testWidgets('when has multiple block child', ((tester) async {
-    await tester.pumpWidget(
-      const Directionality(
+    await tester.pumpWidget(_createWidget('bar\n# Foo'));
+
+    final childWidgets = _descendant();
+    final lastIndex = childWidgets.length - 1;
+
+    expect(childWidgets.elementAt(lastIndex - 2).runtimeType, Padding);
+    expect(childWidgets.elementAt(lastIndex - 1).runtimeType, Column);
+  }));
+
+  testWidgets('when not selectable', ((tester) async {
+    await tester.pumpWidget(_createWidget('Foo', selectable: false));
+    final childWidgets = _descendant();
+    expect(childWidgets.first.runtimeType, Column);
+  }));
+}
+
+Widget _createWidget(
+  String markdown, {
+  bool selectable = true,
+}) =>
+    MaterialApp(
+      home: Directionality(
         textDirection: TextDirection.ltr,
-        child: MarkdownViewer('bar\n# Foo'),
+        child: MarkdownViewer(
+          markdown,
+          selectionColor: selectable ? Colors.green : null,
+        ),
       ),
     );
 
-    final allWidgets = tester.allWidgets;
-
-    // This column is from MarkdownViewer widget.
-    expect(allWidgets.elementAt(2).runtimeType, Column);
-
-    // This column is from MarkdownBuilder.
-    expect(allWidgets.elementAt(3).runtimeType, Column);
-  }));
-}
+Iterable<Widget> _descendant() => collectAllElementsFrom(
+      find.byType(MarkdownViewer).evaluate().single,
+      skipOffstage: false,
+    ).map((e) => e.widget);
