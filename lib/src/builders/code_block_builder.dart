77059f3531f8966bd49794e7209270e8d8c75ab2
chenzhiguang <zhiguang.c@gmail.com>
2022-11-25 19:27:39 +0100

    improve code block style

--- lib/src/builders/code_block_builder.dart
+++ lib/src/builders/code_block_builder.dart
@@ -56,7 +56,7 @@ class CodeBlockBuilder extends MarkdownElementBuilder {
 
   @override
   Widget buildWidget(element, parent) {
-    const defaultPadding = EdgeInsets.all(8.0);
+    const defaultPadding = EdgeInsets.all(15.0);
     final defaultDecoration = BoxDecoration(
       color: const Color(0xfff0f0f0),
       borderRadius: BorderRadius.circular(5),
